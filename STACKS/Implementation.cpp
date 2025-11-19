80e8a70a0251b3f350b83fdf231374854101206b
himanshimittal528@gmail.com <youremail@example.com>
2025-11-19 20:59:04 +0530

    Initial commit

---
+++ STACKS/Implementation.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include<list>
+using namespace std;
+
+/*//ARRAY
+class Stack
+{
+    public:
+    vector<int> v;
+    void push_back(int val)
+    {
+        v.push_back(val);
+    }
+    void pop()
+    {
+        v.pop_back();
+    }
+    int top()
+    {
+        return v[v.size() - 1];
+    }
+};*/
+
+
+
+
+/*  //Linked list
+class Stack{
+    public:
+    list<int>ll;
+    void push_back(int val){
+        ll.push_front(val);
+    }
+    void pop(){
+        ll.pop_front();
+    }
+    int top(){
+        return ll.front();
+    }
+};
+
+int main()
+{
+    Stack s;
+    s.push_back(11);
+    s.push_back(12);
+    s.push_back(13);
+    cout<<s.top()<<endl;
+    s.pop();
+    cout<<s.top()<<endl;
+    return 0;
+}*/
